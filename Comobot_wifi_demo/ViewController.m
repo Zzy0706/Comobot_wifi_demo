7fbbbc7b9c9d3de3e3def49399e6c7a71965e205
Yu.Zhang <Eren.Zhang@zzydeMacBook-Pro.local>
2017-06-27 09:11:09 +0800

    json

--- Comobot_wifi_demo/ViewController.m
+++ Comobot_wifi_demo/ViewController.m
@@ -15,9 +15,25 @@
 #define SOCKET_CMD_INIT    @"010\r\n"
 #define SOCKET_CMD_KEEP    @"ping\r\n"
 @interface ViewController ()<UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate,GCDAsyncSocketDelegate>{
+    BOOL userYou;
+    BOOL flameout;
+    NSString *timStr;
+    NSUserDefaults *user;
+    NSString *getTime;
+    NSString *getMiles;
     NSArray * letter;
     NSString *pickStr;
     UITextField *textF;
+    UITextField *textF1;
+    UITextField *textF2;
+    UITextField *textF3;
+    UITextField *textF4;
+    UITextField *textF5;
+    UITextField *textF6;
+    UITextField *textF7;
+    UITextView *textF8;
+    UITextView *textF9;
+    UITextView *textF10;
 }
 
 @end
@@ -27,22 +43,21 @@
 {
     self.myScrollView.contentSize = CGSizeMake(300,1080);
 }
--(NSThread *)thread{
-    if (!_thread) {
-        _thread = [[NSThread alloc]initWithTarget:self selector:@selector(threadStart) object:nil];
-    }
-    return _thread;
-}
+
+
+
 -(void)threadStart{
     @autoreleasepool {
         [NSTimer scheduledTimerWithTimeInterval:10.0 target:self selector:@selector(heartBeat) userInfo:nil repeats:YES];
         [[NSRunLoop currentRunLoop]run];
     }
+    
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    pickStr = [[NSString alloc]init];
+    userYou = true;
+    user = [NSUserDefaults standardUserDefaults];
+    [self getUserData];
     [self.myScrollView setBackgroundColor:[UIColor blackColor]];
     [self creatText];
     NSLog(@"%@",[[self getCurrentWifiIP] substringToIndex:11]);
@@ -82,16 +97,16 @@
 
 -(void)creatText{
      CGRect r = [ UIScreen mainScreen ].bounds;
-     UITextField *textF1 = [[UITextField alloc]init];
-     UITextField *textF2 = [[UITextField alloc]init];
-     UITextField *textF3 = [[UITextField alloc]init];
-     UITextField *textF4 = [[UITextField alloc]init];
-     UITextField *textF5 = [[UITextField alloc]init];
-     UITextField *textF6 = [[UITextField alloc]init];
-     UITextField *textF7 = [[UITextField alloc]init];
-     UITextView *textF8 = [[UITextView alloc]init];
-     UITextView *textF9 = [[UITextView alloc]init];
-     UITextView *textF10 = [[UITextView alloc]init];
+     textF1 = [[UITextField alloc]init];
+     textF2 = [[UITextField alloc]init];
+     textF3 = [[UITextField alloc]init];
+     textF4 = [[UITextField alloc]init];
+     textF5 = [[UITextField alloc]init];
+     textF6 = [[UITextField alloc]init];
+     textF7 = [[UITextField alloc]init];
+     textF8 = [[UITextView alloc]init];
+     textF9 = [[UITextView alloc]init];
+     textF10 = [[UITextView alloc]init];
     [self.myScrollView addSubview:textF1];
     [self.myScrollView addSubview:textF2];
     [self.myScrollView addSubview:textF3];
@@ -125,16 +140,16 @@
     textF8.userInteractionEnabled = NO;
     textF9.userInteractionEnabled = NO;
     textF10.userInteractionEnabled = NO;
-    [textF1 setText:@"Engine powe status >xxxxxxxxx"];
+    [textF1 setText:@"Engine powe status >"];
     [textF2 setText:@"Miles driven > 0.7(km) 0.4(mile)"];
     [textF3 setText:@"Engine hours > 0.7(min) "];
-    [textF4 setText:@"Total engine hours > xxxxxxx"];
-    [textF5 setText:@"Engine RPM > XXXXXXXX"];
-    [textF6 setText:@"Odometer > xxxxxxxx"];
-    [textF7 setText:@"GPS data > xxxxxxxx"];
-    [textF8 setText:@"OBD data >\n xxxxxxxxxxx "];
-    [textF9 setText:@"Fault code >\n xxxxxxxxx"];
-    [textF10 setText:@"Engine power status>\n xxxxxxx"];
+    [textF4 setText:@"Total engine hours > "];
+    [textF5 setText:@"Engine RPM > "];
+    [textF6 setText:@"Odometer > "];
+    [textF7 setText:@"GPS data > "];
+    [textF8 setText:@"OBD data >\n  "];
+    [textF9 setText:@"Fault code >\n "];
+    [textF10 setText:@"Mileage>\n "];
     [textF1 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.myScrollView);
         make.top.equalTo(self.myScrollView).with.offset(15);
@@ -198,7 +213,8 @@
     return true;
 }
 - (IBAction)startNow:(UIButton *)sender {
-    
+
+
     NSLog(@"-------%@",[[self getCurrentWifiIP] substringToIndex:11]);
     self.clinetSocket= [[GCDAsyncSocket alloc]initWithDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)];
     if ([[[self getCurrentWifiIP] substringToIndex:10] isEqualToString:@"192.168.43"]) {
@@ -211,24 +227,94 @@
 -(void)heartBeat{
     [self.clinetSocket writeData:[self Data:SOCKET_CMD_KEEP] withTimeout:-1 tag:127];
 }
+//将字符转换为数据
 -(NSData *)Data:(NSString *)str{
     NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
     return data;
 }
 - (void)socket:(GCDAsyncSocket*)sock didConnectToHost:(NSString*)host port:(uint16_t)port{
     NSLog(@"成功");
+    NSThread *thread = [[NSThread alloc]initWithTarget:self selector:@selector(threadStart) object:nil];
+    [thread start];
     [self.clinetSocket writeData:[self Data:SOCKET_CMD_INIT] withTimeout:-1 tag:10];
-    [self.thread start];
     [self.clinetSocket readDataWithTimeout:-1 tag:10];
 }
 - (void)socket:(GCDAsyncSocket*)sock didReadData:(NSData*)data withTag:(long)tag{
     NSString *text = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"%@",text);
+    NSArray *arr =[text componentsSeparatedByString:@"|"];
+    NSString *str1 = arr[0];
+    if (arr.count == 2) {
+        if ([str1 containsString:@"@1"]) {
+            if ([arr[1] containsString:@"FLAMEOUT"]) {
+                [user setObject:timStr forKey:@"FLAMEOUT2"];
+                flameout = true;
+            }
+            
+        }else if([str1 containsString:@"@2"]){
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [textF9 setText:[NSString stringWithFormat:@"Fault code >\n %@",arr[1]]];
+            });
+        }else if([str1 containsString:@"@3"]){
+            [self getUserData];
+            NSDictionary *dict = [self DictionFjson:arr[1]];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [textF8 setText:[NSString stringWithFormat:@"OBD data >\n %@ ",arr[1]]];
+            });
+            [self showDataNow:dict];
+        }else if([str1 containsString:@"@4"]){
+            NSDictionary *dict = [self DictionFjson:arr[1]];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [user setObject:dict forKey:@"FLAMEOUT"];
+                });
+            
+        }
+    }
+    
      [self.clinetSocket readDataWithTimeout:-1 tag:10];
 }
+-(NSDictionary *)DictionFjson:(NSString *)jsonStr{
+    if (jsonStr == nil) {
+        return nil;
+    }
+    
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&err];
+    if(err) {
+        NSLog(@"json解析失败：%@",err);
+        return nil;
+    }
+    
+    return dic;
+}
 - (IBAction)stopNow:(UIButton *)sender {
+    [self.clinetSocket disconnect];
     
-    
+}
+-(void)showDataNow:(NSDictionary *)dict{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        int time =[dict[@"TIMES"] floatValue];
+        if (getTime == nil) {
+            getTime = @"0";
+        }
+        float tTime = time/3600+[getTime floatValue];
+        float kmT = [dict[@"MILES-T"] floatValue];
+        float tkmT =kmT/1000 +[getMiles floatValue];
+        timStr = [NSString stringWithFormat:@"%f",tkmT];
+        
+        
+
+        [textF1 setText:[NSString stringWithFormat:@"Engine powe status > Engine powe On"]];
+        [textF2 setText:[NSString stringWithFormat:@"Miles driven > %.01f(km) %.01f(mile)",kmT/1000,kmT*0.000621]];
+        [textF3 setText:[NSString stringWithFormat:@"Engine hours > %d(min) ",time/60]];
+        [textF4 setText:[NSString stringWithFormat:@"Total engine hours > %0.1f(H)",tTime]];
+        [textF5 setText:[NSString stringWithFormat:@"Engine RPM > %@",dict[@"RPM"]]];
+        [textF6 setText:[NSString stringWithFormat:@"Odometer > %.1f(km) %.01f(mile)",tkmT,tkmT*0.621]];
+        [textF7 setText:[NSString stringWithFormat:@"GPS data > "]];
+        [textF10 setText:[NSString stringWithFormat:@"Mileage>\n "]];
+
+    });
 }
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err;{
     if (err) {
@@ -280,8 +366,17 @@
     textF.borderStyle = UITextBorderStyleLine;
     //添加确定到UIAlertController中
     UIAlertAction *OKAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        userYou = false;
         NSLog(@"%@------%@",pickStr,textF.text);
         pickStr=nil;
+        if (!flameout) {
+            [user setObject:textF.text forKey:@"false"];
+        }else{
+            [user setObject:textF.text forKey:@"FLAMEOUT2"];
+        }
+    dispatch_async(dispatch_get_main_queue(), ^{
+             [textF6 setText:[NSString stringWithFormat:@"Odometer > %.1f(km) %.01f(mile)",[textF.text floatValue],[textF.text floatValue]*0.621]];
+        });
         
     }];
     [alerC addAction:OKAction];
@@ -310,5 +405,20 @@
     NSString * title = nil;
     title = letter[row];
     return title;
+}
+-(void)getUserData{
+    NSDictionary *dic = [user objectForKey:@"FLAMEOUT"];
+    getTime = dic[@"TIMES"];
+    if (userYou) {
+        getMiles = dic[@"MILES"];
+    }else if (flameout){
+        getMiles =[user objectForKey:@"FLAMEOUT2"];
+        
+    }else{
+        getMiles =[user objectForKey:@"false"];
+
+    }
+  
+
 }
 @end
